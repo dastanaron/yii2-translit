06e15b5551892750988b45e25cfa4fd5d558beab
dastanaron <flow199@yandex.ru>
2017-10-12 12:23:06 +0300

    started-version

---
+++ README.md
@@ -0,0 +1,41 @@
+Transliterator
+==============
+Transliteration symbols rus-en and en-rus
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require dastanaron/yii2-translit "*"
+```
+
+or add
+
+```
+"dastanaron/yii2-translit": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Для использования можно задать методу translit направление транслитерации:
+ru-en либо en-ru, это не обязательный параметр, можно не указывать,
+в таком случае он сам определит направление в зависимости от языка и переведет его в другой.
+
+```php
+use dastanaron\translit\Translit;
+
+$str = 'Привет МИР';
+
+$translit = new Translit();
+
+echo $translit->translit($str, 'ru-en');
+
+```
---
+++ Translit.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace dastanaron\translit;
+
+/**
+ * This is just an example.
+ */
+class Translit
+{
+
+    public $rus;
+    public $lat;
+    private $route;
+    
+    public function __construct() {
+        $this->rus = [
+            'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+            'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т',
+            'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь',
+            'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё',
+            'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
+            'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+            'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' '
+        ];
+        $this->lat = [
+            'A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I',
+            'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T',
+            'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y',
+            'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e',
+            'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o',
+            'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh',
+            'sch', 'y', 'y', 'y', 'e', 'yu', 'ya', '_'
+        ];
+    }
+    
+    public function translit($str, $route='')
+    {
+       $string = '';
+       if(empty($route)) {
+            $this->route($str);
+       }
+       else {
+           $this->route = $route;
+       }
+       
+       if($this->route == 'en-ru') {
+           $string = str_replace($this->lat, $this->rus, $str);
+       }
+       elseif($this->route == 'ru-en') {
+           $string = str_replace($this->rus, $this->lat, $str);
+       }
+       else {
+           throw new Exception('Неизвестное направление транслитерации');
+       }       
+       
+       return $string;
+       
+    }
+    
+    private function route($str)
+    {
+        if(preg_match('#[A-Za-z]+#', $str)) {
+            $this->route = 'en-ru';
+        }
+        else {
+            $this->route = 'ru-en';
+        }
+    }
+    
+    
+}
